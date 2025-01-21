45108cfa239c3b3f3d87afdc901146e2752923ed
Prasanna-2005-13 <dubbakaprasanna13@gmail.com>
2025-01-21 20:46:32 +0530

    Day 8 files are added edited

--- async.js
+++ async.js
@@ -10,7 +10,26 @@ setInterval(()=>{
 console.log(40);
 console.log(50);
 
+
 //! promise
 
-let p1 = new Promise((resolve,reject)=>{});
-console.log(p1);
+// let p1 = new Promise((resolve , reject)=>{});
+// console.log(p1);
+
+let p2 = new Promise((resolve , reject)=>{
+  resolve("success");
+})
+// console.log(p2);
+// p2
+// .then((response)=>console.log(response))
+// .catch((error)=>console.log(error))
+// .finally(()=>console.log("finally for both"))
+
+let p3 = new Promise((resolve , reject)=>{
+  reject("Failure");
+});
+// console.log(p3);
+p3
+.then(data=>console.log(data))
+.catch(err=>console.log(err))
+.finally(()=>console.log("finally for both"))
