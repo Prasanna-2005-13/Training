6c7edf796b6769ee105357ce92e6dcb8be720fc1
Prasanna-2005-13 <dubbakaprasanna13@gmail.com>
2025-01-21 20:17:15 +0530

    Day 8 files are added

---
+++ async.js
@@ -0,0 +1,16 @@
+console.log(10);
+console.log(20);
+//setTimeout(()=>{
+  //  console.log(30); 
+//},5000)
+setInterval(()=>{
+    //document.writeln("MRU");
+    console.log(30);
+},1500)
+console.log(40);
+console.log(50);
+
+//! promise
+
+let p1 = new Promise((resolve,reject)=>{});
+console.log(p1);
