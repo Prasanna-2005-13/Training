635df3cb21f2bf6a9add95b7a791b6dc941760fd
Prasanna-2005-13 <Dubbakaprasanna13@gmail.com>
2025-01-27 19:48:51 +0530

    Add files via upload
    Day 13 files are added

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-// console.log(window);
+/*// console.log(window);
 // console.log(document);
 
 // let element = document.getElementById("demo");
@@ -139,3 +139,82 @@ form.addEventListener("submit",(event)=>{
 })
 
 // localStorage.clear();
+
+// ! 
+
+// let vd= document.createElement("video");
+// vd.setAttribute("src" ,"./video123.mp4");
+// vd.setAttribute("controls" , "controls");
+// console.log(vd);
+
+
+// document.body.appendChild(vd)
+
+// let h1 = document.createElement("h1");
+// h1.setAttribute("id","demo")
+// h1.innerText = "MRU Batch3 JS session";
+// console.log(h1);
+// h1.removeAttribute("id");
+
+// document.body.appendChild(h1);
+
+
+// ! 
+// let mainEle = document.createElement("div");
+// mainEle.setAttribute("id","mainBlock");
+// mainEle.style.border="2px solid red";
+// mainEle.style.width="550px";
+// mainEle.style.height="550px";
+
+// console.log(mainEle);
+
+
+// let topEle = document.createElement("div");
+// topEle.setAttribute("class","topBlock");
+// // console.log(topEle);
+
+// let image = document.createElement("img");
+// image.src="https://cdn.pixabay.com/photo/2023/02/09/16/43/cat-7779237_640.jpg";
+// image.style.width="550px"
+
+// let bottomEle = document.createElement("div");
+// bottomEle.setAttribute("class","bottomBlock");
+// // console.log(bottomEle);
+
+// let h1 = document.createElement("h1");
+// h1.innerText = "CAT";
+// h1.style.textAlign="center"
+
+// let btn = document.createElement("button");
+// btn.innerText="View More";
+// btn.style.border="none";
+// btn.style.padding="20px";
+// btn.style.backgroundColor="dodgerblue";
+// btn.style.color="white";
+
+// bottomEle.appendChild(h1);
+// bottomEle.appendChild(btn);
+
+// topEle.appendChild(image);
+// mainEle.appendChild(topEle);
+// mainEle.appendChild(bottomEle);
+// document.body.appendChild(mainEle);
+
+*/
+
+let form = document.querySelector("form");
+
+let username = document.getElementById("uName");
+let password = document.getElementById("uPass");
+
+let gender = document.getElementsByName("gender");
+console.log(gender);    
+
+form.addEventListener("submit" , event=>{
+    event.preventDefault();
+    let un = username.value;
+    let up = password.value;
+    let gen = gender.value;
+
+    console.log(un , up , gen);
+})
