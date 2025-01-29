2924ad34cf3ea8fc9f5ad8352a6c827485678ab4
Prasanna-2005-13 <Dubbakaprasanna13@gmail.com>
2025-01-29 15:30:38 +0530

    Day 15 files are Added

---
+++ Assignment1.js
@@ -0,0 +1,15 @@
+let userDetails = {
+    empid : "1",
+    empGender : "Male",
+    empcompany :"Amazon",
+    empsalary : "30,000",
+    empaddress : "Hyderabad",
+    empaddress:{
+        city:"Hyd",
+        area :{
+            areaName:"Hitech",
+            pincode :123456
+        }
+    }
+}
+console.log(userDetails)
